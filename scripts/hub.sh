93b01d63ca8a24dd1a68b445bffd4601b1cbe807
Gerrit Riessen <gorenje@posteo.de>
2021-03-14 12:11:50 +0100

    typos in script

--- scripts/hub.sh
+++ scripts/hub.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-## Entrypoint for the DockerHub image. Allows for deactivating the Jupyter
-## token.
+## Entrypoint for the DockerHub image.
 
+## Allows for deactivation of the Jupyter token.
 if [ "${NO_TOKEN}" == 'yes' ]; then
-    echo "DELETEING JUPYTER TOKEN"
+    echo "DELETING JUPYTER TOKEN"
     jupyter notebook --generate-config
     echo "c.NotebookApp.token = ''" >> ${HOME}/.jupyter/jupyter_notebook_config.py
 fi
 
+## Start up Jupyter lab.
 jupyter lab --ip 0.0.0.0 --port 8888
