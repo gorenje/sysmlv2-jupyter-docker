8885271a4725d7f38f33c6a0520c0fb6bd422f23
Gerrit Riessen <gorenje@posteo.de>
2021-03-14 17:17:55 +0100

    DockerHub: image now opens with the readme

--- scripts/hub.sh
+++ scripts/hub.sh
@@ -9,6 +9,12 @@ if [ "${NO_TOKEN}" == 'yes' ]; then
     echo "c.NotebookApp.token = ''" >> ${HOME}/.jupyter/jupyter_notebook_config.py
 fi
 
+## Override the default workspace of 'lab' and make it open the readme.
+mkdir -p /home/sysml/.jupyter/lab/workspaces
+
+echo '{"data":{"layout-restorer:data":{"main":{"dock":{"type":"tab-area","currentIndex":1,"widgets":["notebook:README-SysMLv2Lab.ipynb"]},"mode":"multiple-document","current":"notebook:README-SysMLv2Lab.ipynb"},"left":{"collapsed":false,"current":"filebrowser","widgets":["filebrowser","running-sessions","command-palette","jp-property-inspector","tab-manager","extensionmanager.main-view"]},"right":{"collapsed":true,"widgets":[]}},"notebook:README-SysMLv2Lab.ipynb":{"data":{"path":"README-SysMLv2Lab.ipynb","factory":"Notebook"}}},"metadata":{"id":"/lab"}}' >> /home/sysml/.jupyter/lab.json
+
+jupyter lab workspaces import /home/sysml/.jupyter/lab.json
 
 ## Start up Jupyter lab.
 jupyter lab --ip 0.0.0.0 --port 8888 --no-browser
